20b96d5eb76f117bf6850f9a916ad2d0c98cec69
luiz <luiz@xps>
2010-04-23 13:48:53 +0300

    Adding basic browsing and first part of lunches creation

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,32 @@
+import org.joda.time.LocalDateTime
+import org.joda.time.LocalDate
+import org.joda.time.LocalTime
+
+import com.wannalunch.domain.Lunch
+
 class BootStrap {
 
-     def init = { servletContext ->
-     }
-     def destroy = {
-     }
+  def init = { servletContext ->
+    Lunch lunch1 = new Lunch()
+    lunch1.topic = "Topic 1"
+    lunch1.description = "Desc 1"
+    lunch1.createDateTime = new LocalDateTime().minusHours(1)
+    lunch1.date = new LocalDate()
+    lunch1.time = new LocalTime()
+    lunch1.place = "Vapiano"
+    	
+    Lunch lunch2 = new Lunch()
+    lunch2.topic = "Topic 2"
+    lunch2.description = "Desc 2"
+    lunch2.createDateTime = new LocalDateTime()
+    lunch2.date = new LocalDate()
+    lunch2.time = new LocalTime()
+    lunch2.place = "Vapiano"
+    
+    lunch1.save()
+    lunch2.save()
+  }
+
+  def destroy = {
+  }
 } 
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -12,11 +12,11 @@ grails.project.dependency.resolution = {
     repositories {        
         grailsPlugins()
         grailsHome()
-
+        mavenCentral()
+        
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
         //mavenLocal()
-        //mavenCentral()
         //mavenRepo "http://snapshots.repository.codehaus.org"
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
@@ -26,6 +26,7 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         // runtime 'mysql:mysql-connector-java:5.1.5'
+    	compile 'joda-time:joda-time:1.6'
     }
 
 }
